2c430dae1c8d81fefac2adee1c8dcdcf23019566
akivabuckman <akivabuckman@gmail.com>
2026-01-21 15:58:35 +0200

    vite config fix2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    base: '/chord-finder/',
 });
