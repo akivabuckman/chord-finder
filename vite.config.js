31b55f57d45bfc5ea53c3cd1d532f4c097e07732
akivabuckman <akivabuckman@gmail.com>
2026-03-10 19:57:55 +0200

    npm audit

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// filepath: /c:/Users/akiva/coding/personal/chord-finder/vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    base: '/chord-finder/',
-});
